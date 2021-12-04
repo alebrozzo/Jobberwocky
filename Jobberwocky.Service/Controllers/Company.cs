623934eb5e300dc55183e71b224818647479bbd2
Alejandro Brozzo <alejandro.brozzo@aranzmedical.com>
2021-12-04 22:02:06 +1300

    Initial application setup

---
+++ Jobberwocky.Service/Controllers/Company.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Jobberwocky.Service.Controllers
+{
+  [Route("api/companies")]
+  [ApiController]
+  public class Company : ControllerBase
+  {
+    [HttpGet]
+    public List<string> Get()
+    {
+      return new List<string>() { "Company 1", "Company 2" };
+    }
+
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
