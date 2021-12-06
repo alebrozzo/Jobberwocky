e803e269ba7ff62091067bc35edbb59900d5d467
Alejandro Brozzo <alejandro.brozzo@aranzmedical.com>
2021-12-06 18:50:08 +1300

    Company Service implementation

--- Jobberwocky.Service/Services/CompanyService.cs
+++ Jobberwocky.Service/Services/CompanyService.cs
@@ -1,4 +1,7 @@
-﻿using Jobberwocky.DataAccess;
+﻿using System;
+using System.Threading.Tasks;
+using Jobberwocky.Api.Services.OperationHandling;
+using Jobberwocky.DataAccess;
 using Jobberwocky.Domain;
 
 namespace Jobberwocky.Api.Services
@@ -12,9 +15,100 @@ namespace Jobberwocky.Api.Services
       this.companyRepository = companyRepository ?? throw new System.ArgumentNullException(nameof(companyRepository));
     }
 
-    public Company Add(Company company)
+    public async Task<OperationResult<Company>> Get(Guid id)
     {
-      return this.companyRepository.Add(company);
+      if (id == Guid.Empty)
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Please indicate an id to search by.");
+      }
+
+      var company = await this.companyRepository.Get(id);
+      if (company == null)
+      {
+        return OperationResult<Company>.Error(OperationStatus.NotFound);
+      }
+
+      return OperationResult<Company>.Ok(company);
+    }
+
+    public async Task<OperationResult<Company>> Add(Company company)
+    {
+      var validationResult = this.ValidateCompany(company);
+      if (validationResult != null)
+      {
+        return validationResult;
+      }
+
+      var companyId = await this.companyRepository.Add(company);
+      var retrievedCompany = await this.companyRepository.Get(companyId);
+
+      return OperationResult<Company>.Ok(retrievedCompany);
+    }
+
+    public async Task<OperationResult<Company>> Update(Company company)
+    {
+      var validationResult = this.ValidateCompany(company);
+      if (validationResult != null)
+      {
+        return validationResult;
+      }
+
+      if (company.Id == Guid.Empty)
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Please provide the ID of the company to update.");
+      }
+
+      await this.companyRepository.Update(company);
+      var retrievedCompany = await this.companyRepository.Get(company.Id);
+
+      if (retrievedCompany == null)
+      {
+        return OperationResult<Company>.Error(OperationStatus.NotFound);
+      }
+
+      return OperationResult<Company>.Ok(retrievedCompany);
+    }
+
+    public async Task<OperationResult<bool>> Delete(Guid id)
+    {
+      if (id == Guid.Empty)
+      {
+        return OperationResult<bool>.Error(OperationStatus.ValidationError, "Please provide the ID of the company to delete.");
+      }
+
+      var retrievedCompany = await this.companyRepository.Get(id);
+      if (retrievedCompany == null)
+      {
+        return OperationResult<bool>.Error(OperationStatus.NotFound);
+      }
+
+      await this.companyRepository.Delete(id);
+      return OperationResult<bool>.Ok(true);
+    }
+
+    private OperationResult<Company> ValidateCompany(Company company)
+    {
+      if (company == null)
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Company data is missing.");
+      }
+
+      if (string.IsNullOrWhiteSpace(company.Name))
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Company name is missing.");
+      }
+
+      if (company.Name.Length < 5 || company.Name.Length > 20)
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Company name must be between 5 and 20 characters long.");
+      }
+
+      if (company.Description != null && company.Description.Length > 500)
+      {
+        return OperationResult<Company>.Error(OperationStatus.ValidationError, "Company description must be up to 500 characters long.");
+      }
+
+      return null;
     }
   }
 }
