f7e727f1447a8728d3961f05d5729486edb65dda
Alejandro Brozzo <alejandro.brozzo@aranzmedical.com>
2021-12-06 18:01:43 +1300

    Completed Company repository

--- Jobberwocky.DataAccess/Company/CompanyRepository.cs
+++ Jobberwocky.DataAccess/Company/CompanyRepository.cs
@@ -1,14 +1,48 @@
-﻿using Jobberwocky.Domain;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jobberwocky.Domain;
 
 namespace Jobberwocky.DataAccess
 {
   public class CompanyRepository : ICompanyRepository
   {
     // For the sake of brevity, I am using an In Memory data repository
+    // await Task.Yield(); fakes the asyncrony expected from a database or external api call
+    private static readonly List<Company> companies = new List<Company>();
 
-    public Company Add(Company company)
+    public async Task<Company> Get(Guid id)
     {
-      throw new System.NotImplementedException();
+      await Task.Yield();
+      return companies.FirstOrDefault(c => c.Id == id);
+    }
+
+    public async Task<Guid> Add(Company company)
+    {
+      await Task.Yield();
+      if (company.Id == Guid.Empty)
+      {
+        company.Id = Guid.NewGuid();
+      }
+
+      companies.Add(company);
+
+      return company.Id;
+    }
+
+    public async Task Update(Company company)
+    {
+      await Task.Yield();
+      var ix = companies.FindIndex(c => c.Id == company.Id);
+      companies[ix] = company;
+    }
+
+    public async Task Delete(Guid id)
+    {
+      await Task.Yield();
+      var ix = companies.FindIndex(c => c.Id == id);
+      companies.RemoveAt(ix);
     }
   }
 }
--- Jobberwocky.DataAccess/Company/ICompanyRepository.cs
+++ Jobberwocky.DataAccess/Company/ICompanyRepository.cs
@@ -1,9 +1,37 @@
-﻿using Jobberwocky.Domain;
+﻿using System;
+using System.Threading.Tasks;
+using Jobberwocky.Domain;
 
 namespace Jobberwocky.DataAccess
 {
   public interface ICompanyRepository
   {
-    public Company Add(Company company);
+    /// <summary>
+    /// Gets a company by ID.
+    /// </summary>
+    /// <param name="id">The ID of the company to retrieve.</param>
+    /// <returns></returns>
+    Task<Company> Get(Guid id);
+
+    /// <summary>
+    /// Adds a company to the repository.
+    /// </summary>
+    /// <param name="company">The data to store.</param>
+    /// <returns>The id of the new company. Can be passed in parameter or automatically generated.</returns>
+    Task<Guid> Add(Company company);
+
+    /// <summary>
+    /// Updates an existing company. Searches by ID.
+    /// </summary>
+    /// <param name="company">The new data for the company. ID must exist.</param>
+    /// <returns></returns>
+    Task Update(Company company);
+
+    /// <summary>
+    /// Deletes a company.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task Delete(Guid id);
   }
 }
