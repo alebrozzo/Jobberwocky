a1da3f3f8094af23a973a7d6b06e4c0e276d57f1
Alejandro Brozzo <alejandro.brozzo@aranzmedical.com>
2021-12-08 19:25:56 +1300

    Fake repo data

--- Jobberwocky.DataAccess/Company/CompanyRepository.cs
+++ Jobberwocky.DataAccess/Company/CompanyRepository.cs
@@ -10,7 +10,13 @@ namespace Jobberwocky.DataAccess
   {
     // For the sake of brevity, I am using an In Memory data repository
     // await Task.Yield(); fakes the asyncrony expected from a database or external api call
-    private static readonly List<Company> companies = new List<Company>();
+    private static readonly List<Company> companies = new List<Company>
+    {
+      new Company { Id = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-000000000000"), Name = "Avature", Description = "The coolest company." },
+      new Company { Id = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-111111111111"), Name = "Netflix", Description = "You'll work with us and then go home and still be with us." },
+      new Company { Id = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-222222222222"), Name = "Glovo", Description = "Bike your way up." },
+      new Company { Id = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-333333333333"), Name = "ARANZ Medical", Description = "A health technology company." },
+    };
 
     public async Task<Company> Get(Guid id)
     {
--- Jobberwocky.DataAccess/Company/PostingRepository.cs
+++ Jobberwocky.DataAccess/Company/PostingRepository.cs
@@ -10,7 +10,64 @@ namespace Jobberwocky.DataAccess
   {
     // For the sake of brevity, I am using an In Memory data repository
     // await Task.Yield(); fakes the asyncrony expected from a database or external api call
-    private static readonly List<Posting> postings = new List<Posting>();
+    private static readonly List<Posting> postings = new List<Posting>
+    {
+      new Posting {
+        Id = Guid.Parse("00000000-bbbb-cccc-dddd-000000000000"),
+        Title = "Sr. Software Engineer",
+        Location = "Christchurch, New Zealand",
+        CompanyId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-333333333333"),
+        Description = "A great software engineer and person to cover for a recent loss.",
+        RemoteAvailable = true,
+        SalaryRangeMin = 90000,
+        SalaryRangeMax = 110000,
+        Tags = new List<string> { "C#", "Agile", "Soft Skills" },
+      },
+      new Posting {
+        Id = Guid.Parse("11111111-bbbb-cccc-dddd-000000000000"),
+        Title = "Software Engineer",
+        Location = "Barcelona, España",
+        CompanyId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-222222222222"),
+        Description = "Main Responsibilities and Duties: blah blah blah",
+        RemoteAvailable = true,
+        SalaryRangeMin = 40000,
+        SalaryRangeMax = 55000,
+        Tags = new List<string> { "C#", ".NET", "TypeScript", "Angular" },
+      },
+      new Posting {
+        Id = Guid.Parse("22222222-bbbb-cccc-dddd-000000000000"),
+        Title = "Team Leader",
+        Location = "Madrid, España",
+        CompanyId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-222222222222"),
+        Description = "Main Responsibilities and Duties: blah blah blah",
+        RemoteAvailable = true,
+        SalaryRangeMin = 45000,
+        SalaryRangeMax = 65000,
+        Tags = new List<string> { "C#", ".NET", "Soft Skills" },
+      },
+      new Posting {
+        Id = Guid.Parse("33333333-bbbb-cccc-dddd-000000000000"),
+        Title = "Account Manager",
+        Location = "Madrid, España",
+        CompanyId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-111111111111"),
+        Description = "Needs to manage accounts like a pro",
+        RemoteAvailable = false,
+        SalaryRangeMin = 45000,
+        SalaryRangeMax = 65000,
+        Tags = new List<string> { "Management", "Soft Skills" },
+      },
+      new Posting {
+        Id = Guid.Parse("44444444-bbbb-cccc-dddd-000000000000"),
+        Title = "Sr. Software Engineer",
+        Location = "Málaga, España",
+        CompanyId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-000000000000"),
+        Description = "Needs to manage accounts like a pro",
+        RemoteAvailable = false,
+        SalaryRangeMin = 35000,
+        SalaryRangeMax = 55000,
+        Tags = new List<string> { "C#", ".NET", "TypeScript", "React", "Soft Skills", "JavaScript" },
+      },
+    };
 
     public async Task<Posting> Get(Guid id)
     {
