b5afd1bda54f6c4cdcb53fc977c22541ce6a29f7
Alejandro Brozzo <alejandro.brozzo@aranzmedical.com>
2021-12-11 09:27:23 +1300

    Convert Jobberwacky results to our Posting type

--- Jobberwocky.Service/Services/Postings/JobberwockyExternalProvider.cs
+++ Jobberwocky.Service/Services/Postings/JobberwockyExternalProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Jobberwocky.Api.Dtos;
 using Jobberwocky.Api.Services.OperationHandling;
@@ -32,13 +33,61 @@ namespace Jobberwocky.Api.Services
         return new Posting[0];
       }
 
-      var results = await response.Content.ReadAsStringAsync();
-      return this.Map(results);
+      var resultsAsString = await response.Content.ReadAsStringAsync();
+      var results = this.ToJobberwockyResultDtos(resultsAsString);
+      return results.Select(this.Map);
     }
 
-    private IEnumerable<Posting> Map(string jobberwockyResults)
+    /// <summary>
+    /// Map the received data to a class matching the Jobberwocky result type.
+    /// </summary>
+    /// <param name="jobberwockyResultsAsJson">a json string with the search results that will be mapped to an enumerable of <see cref="JobberwockyResultDto"/>.</param>
+    /// <returns></returns>
+    private IEnumerable<JobberwockyResultDto> ToJobberwockyResultDtos(string jobberwockyResultsAsJson)
     {
-      throw new NotImplementedException();
+      var resultList = new List<JobberwockyResultDto>();
+      var resultArrays = JsonSerializer.Deserialize<dynamic[]>(jobberwockyResultsAsJson);
+      foreach (var result in resultArrays)
+      {
+        decimal.TryParse(result[1].ToString(), out decimal salary);
+        var skillsAsString = result[3].ToString();
+        resultList.Add(new JobberwockyResultDto
+        {
+          Name = result[0].ToString(),
+          Salary = salary,
+          Country = result[2].ToString(),
+          Skills = JsonSerializer.Deserialize<string[]>(skillsAsString),
+        });
+      }
+
+      return resultList;
+    }
+
+    /// <summary>
+    /// Maps a <see cref="JobberwockyResultDto"/> to a proper <see cref="Posting"/>.
+    /// </summary>
+    /// <param name="jobberwockyDto">The object to map from.</param>
+    /// <returns></returns>
+    private Posting Map(JobberwockyResultDto jobberwockyDto)
+    {
+      return new Posting
+      {
+        Title = jobberwockyDto.Name,
+        SalaryRangeMin = jobberwockyDto.Salary,
+        SalaryRangeMax = jobberwockyDto.Salary,
+        Tags = jobberwockyDto.Skills,
+      };
+    }
+
+    private class JobberwockyResultDto
+    {
+      public string Name { get; set; }
+
+      public string Country { get; set; }
+
+      public decimal Salary { get; set; }
+
+      public IEnumerable<string> Skills { get; set; }
     }
   }
 }
